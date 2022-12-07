ad03df011e3ce7b3238ebf01c7e9c7be03c97172
Matthieu Etchegoyen <etchegom@gmail.com>
2022-12-07 20:50:38 +0100

    Add enhancements (#2)

--- autodisco/apps.py
+++ autodisco/apps.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from django.apps import AppConfig
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.module_loading import autodiscover_modules
 
 
@@ -10,5 +11,9 @@ class AutodiscoConfig(AppConfig):
     name = "autodisco"
 
     def ready(self) -> None:
-        for mod in settings.AUTODISCO_MODULES:
+        if not isinstance(settings.AUTODISCO_MODULES, list):
+            raise ImproperlyConfigured("AUTODISCO_MODULES must be a list")
+        for mod in sorted(
+            [str(m) for m in settings.AUTODISCO_MODULES if m not in ("models", "admin")]
+        ):
             autodiscover_modules(mod)
--- tests/test_apps.py
+++ tests/test_apps.py
@@ -3,19 +3,41 @@ from __future__ import annotations
 from unittest.mock import call, patch
 
 from django.apps import apps
+from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
 
 
 class AutodiscoConfigTest(TestCase):
-    @override_settings(AUTODISCO_MODULES=["module_a", "module_b"])
-    def test_apps(self):
-        autodisco_app = apps.get_app_config("autodisco")
-        with patch("autodisco.apps.autodiscover_modules") as mock_autodiscover:
+    def _get_autodisco_app(self):
+        return apps.get_app_config("autodisco")
+
+    @override_settings(AUTODISCO_MODULES=["module_b", "module_a"])
+    @patch("autodisco.apps.autodiscover_modules")
+    def test_autodiscover_called_on_ready(self, mock_autodiscover):
+        self._get_autodisco_app().ready()
+        mock_autodiscover.assert_has_calls(
+            calls=[
+                call("module_a"),
+                call("module_b"),
+            ],
+        )
+
+    @override_settings(AUTODISCO_MODULES=True)
+    @patch("autodisco.apps.autodiscover_modules")
+    def test_improperly_configured(self, mock_autodiscover):
+        autodisco_app = self._get_autodisco_app()
+        with self.assertRaises(ImproperlyConfigured):
             autodisco_app.ready()
-            mock_autodiscover.assert_has_calls(
-                calls=[
-                    call("module_a"),
-                    call("module_b"),
-                ],
-                any_order=True,
-            )
+        mock_autodiscover.assert_not_called()
+
+    @override_settings(AUTODISCO_MODULES=[123456])
+    @patch("autodisco.apps.autodiscover_modules")
+    def test_non_str_module_name(self, mock_autodiscover):
+        self._get_autodisco_app().ready()
+        mock_autodiscover.assert_called_once_with("123456")
+
+    @override_settings(AUTODISCO_MODULES=["module", "models", "admin"])
+    @patch("autodisco.apps.autodiscover_modules")
+    def test_ignore_reserved_module_names(self, mock_autodiscover):
+        self._get_autodisco_app().ready()
+        mock_autodiscover.assert_called_once_with("module")
